5cc23a1aa77568e53a207a34910ebbf302af0c5c
chrisftian <chrisftian@tencent.com>
2024-06-12 15:20:49 +0800

    fix: 优化代码

--- demo/lib/cos-wx-sdk-v5.js
+++ demo/lib/cos-wx-sdk-v5.js
@@ -14659,8 +14659,9 @@ var arrayBufferToString = function arrayBufferToString(arrayBuffer) {
 var parseResBody = function parseResBody(responseBody) {
   var json;
   if (responseBody && typeof responseBody === 'string') {
-    var isXml = responseBody.trim().indexOf('<') === 0;
-    var isJson = responseBody.trim().indexOf('{') === 0;
+    var trimBody = responseBody.trim();
+    var isXml = trimBody.indexOf('<') === 0;
+    var isJson = trimBody.indexOf('{') === 0;
     if (isXml) {
       // xml 解析，解析失败返回{}
       json = util.xml2json(responseBody) || {};
